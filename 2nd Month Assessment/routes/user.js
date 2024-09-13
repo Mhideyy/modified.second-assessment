7e8d429e52e117001fd075ab1e4e23f29fc15f17
Ifagbuyi samuel <114811594+Mhideyy@users.noreply.github.com>
2024-09-13 22:44:18 +0100

    Delete 2nd Month Assessment directory

--- 2nd Month Assessment/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-const { createUser, loginUser} = require('../controllers/user');
-const verify = require('../middleware/verify');
-
-routes.post('/user', verify, createUser);
-routes.post('/login-user', verify, loginUser);
-
-module.exports = routes;
